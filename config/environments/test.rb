c52c8a17a083e4ac0d1ed52d1d9de7e664794172
Khaaliq DeJan <khaaliqdejan@gmail.com>
2018-09-19 14:25:34 -0700

    setup token based auth for api

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Rails.application.routes.default_url_options[:host]= ENV["HTTP_HOST"] || "localhost"
+Rails.application.routes.default_url_options[:port]= ENV["PORT"] || 3000
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_202349) do
+ActiveRecord::Schema.define(version: 2018_09_19_211522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "news", force: :cascade do |t|
+    t.text "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
